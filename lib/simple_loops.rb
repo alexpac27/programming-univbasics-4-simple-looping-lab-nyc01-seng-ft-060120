552ca47f2b29d7d35c97fb8f8f1ed990af6616aa
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-21 23:42:26 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,17 +4,14 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times(message, limit)
+def loop_message_n_times(message, number)
   count = 0
-  while count <= limit do
+  while count <= number do
     puts message
     count += 1
   end
 end
 
-
-
-#loop_message_n_times(10,"Hello")
 
 def output_array(array)
   counter = 0
